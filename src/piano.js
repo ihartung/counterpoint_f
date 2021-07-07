63efbe6e8e11982e2376b9a886cdfa4da6b73ff4
Isaac Hartung <isaac.hartung@gmail.com>
2021-07-07 16:23:21 -0400

    adding piano keyboard library, adding file to contain it

---
+++ src/piano.js
@@ -0,0 +1,25 @@
+import {Piano, KeyboardShortcuts, MidiNumbers} from 'react-piano';
+import 'react-piano/dist/styles.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
